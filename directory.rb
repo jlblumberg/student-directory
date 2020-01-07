e9679a3a4a7074c9ffd1628bc97cd51e19a24eea
Josh Blumberg <jlblumberg@gmail.com>
2020-01-07 13:08:51 +0000

    changing order of methods

--- directory.rb
+++ directory.rb
@@ -11,19 +11,11 @@ def input_students
   end
 end
 
-def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
-end
-
-def print_students_list
-  @students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
   end
-end
-
-def print_footer
-  puts "Overall, we have #{@students.count} great students"
 end
 
 def print_menu
@@ -51,12 +43,19 @@ def process(selection)
   end
 end
 
-def interactive_menu
-  loop do
-    # 1. print the menu and ask what the user wants to do
-    print_menu
-    process(gets.chomp)
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print_students_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 interactive_menu
